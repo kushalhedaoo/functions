bd68817da5b0290a13d18df1830968a12dcf870a
kushal07 <90027593+kushalhedaoo@users.noreply.github.com>
2023-09-23 10:19:17 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+#
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name='functions',
+    version='0.0.3',
+    author='Kushal Hedaoo',
+    author_email='kushalhedaoo29@gmail.com',
+    description='Simple functioning calculator',
+
+    url='https://github.com/mike-huls/toolbox',
+
+    license='MIT',
+    packages=['functions'],
+    install_requires=['numpy,pandas'],
+)
